e754fff43387ef519295558b5027628b1c09b72f
wwhorton <whorton@gmail.com>
2014-04-09 19:51:30 -0400

    Template stuff

--- app_server.js
+++ app_server.js
@@ -7,19 +7,12 @@ var Post = require('./js/Post.js');
 app.use(express.bodyParser());
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/templates');     
+
 app.get('/', function(request, response){
-        /* Return every post
-          
-        1. Query database with wildcard
-        2. For each post, create post object
-        3. Create view for each post
-        4. Display each view
-                
-        */
         Schemas.Post.find(function(err, posts){
             if (err) return console.error.bind(console, "List All error:");
             if (posts != []) {
-                response.render('post', {posts:posts}); 
+                response.render('layout', {posts:posts}); 
                 
             } else {
                 console.log("Query results empty.");
