1bbfb14002c22abf8b9a6fc51803faa88a597d81
wwhorton <whorton@gmail.com>
2014-04-14 17:25:12 -0400

    Almost...

--- app_server.js
+++ app_server.js
@@ -84,8 +84,9 @@ app.delete('/',  function(request, response){ // Pulled for testing: passport.au
     //if(request.body.username == request.user.username){
         thisPost.remove(thisPost, function(error){
             if (error) console.log("Could not delete post.");
-            response.redirect('http://murmuring-spire-3618.herokuapp.com/');
+           
         });
+         response.redirect('/');
     //}
     
 });
