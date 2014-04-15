dd03208609e293490d74ef61e1b41c5bb4b1e6d9
wwhorton <whorton@gmail.com>
2014-04-14 21:58:59 -0400

    qewfqfe

--- app_server.js
+++ app_server.js
@@ -69,6 +69,7 @@ app.put('/', function(request, response){  // Pulled for testing: passport.authe
         doc.body = request.body.body;
         doc.username = request.body.username;
         doc.save();
+        if(error) console.log(error);
     });
     //response.redirect('/');
 });
