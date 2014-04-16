77288167aec270bc1a629c43e16c74f22677a127
wwhorton <whorton@gmail.com>
2014-04-16 18:26:54 -0400

    Sass update

--- sass/config.rb
+++ sass/config.rb
@@ -1,12 +1,12 @@
 # Require any additional compass plugins here.
-
+require "susy"
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "/css"
 sass_dir = "/sass"
 images_dir = "/img"
 javascripts_dir = "/js"
-fonts_dir = "Fonts"
+fonts_dir = "/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -20,6 +20,7 @@ fonts_dir = "Fonts"
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
- preferred_syntax = :sass
+# preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
